50359a91a1a508d60c5857e67de8dd12576c96d8
George Campbell <geocolumbus@gmail.com>
2024-09-07 12:45:31 -0400

    Let there be light

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+app.config['JSON_AS_ASCII'] = False
+
+@app.route("/", methods=['GET', 'POST'])
+def api():
+    if request.method == 'GET':
+        return "This is the GET"
+    else:
+        return "This is the POST"
+
+if __name__ == '__main__':
+    app.run(debug=True)
